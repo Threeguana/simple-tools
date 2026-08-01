117944ace922325ba46117d92078baf074d93741
Andini <163494512+Threeguana@users.noreply.github.com>
2026-08-01 14:43:29 +0700

    Update fileOrganizer.sh

--- fileOrganizer.sh
+++ fileOrganizer.sh
@@ -1,26 +1,18 @@
 #!/system/bin/sh
 
-# ===========================================================
-#  File Organizer - Android Shell
-# ===========================================================
-
-# Gunakan folder saat ini
 TARGET_DIR="."
 
 cd "$TARGET_DIR" || exit
 
 for file in *; do
-    # Pastikan itu adalah file (bukan folder) dan bukan script ini sendiri
+    
     if [ -f "$file" ] && [ "$file" != "${0##*/}" ]; then
-        # Ambil ekstensi file
         ext="${file##*.}"
         
-        # Jika file tidak memiliki ekstensi
         if [ "$ext" = "$file" ]; then
             ext="unknown"
         fi
         
-        # Buat folder jika belum ada, lalu pindahkan
         [ -d "$ext" ] || mkdir -p "$ext"
         mv "$file" "$ext/"
     fi
